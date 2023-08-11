7c7d6af05816a466f1b687105a32d34c129350e3
Junior31AR <alamaramosjunior315@gmail.com>
2023-08-11 10:16:36 -0500

    Iniciando proyecto con dependencias

---
+++ src/main/java/com/example/comas_educa/ComasEducaApplication.java
@@ -0,0 +1,14 @@
+package com.example.comas_educa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComasEducaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComasEducaApplication.class, args);
+		System.out.println("Running Program");
+	}
+
+}
---
+++ src/test/java/com/example/comas_educa/ComasEducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comas_educa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComasEducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
