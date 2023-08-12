7627fba52c30208c89fe28fb3ad3c1c27ab12b33
Junior31AR <alamaramosjunior315@gmail.com>
2023-08-11 19:30:50 -0500

    Iniciando proyecto

--- src/main/java/com/example/comas_educa/ComasEducaApplication.java
+++ src/main/java/com/example/comas_educa/ComasEducaApplication.java
@@ -8,7 +8,7 @@ public class ComasEducaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComasEducaApplication.class, args);
-		System.out.println("Running Programa");
+		System.out.println("Running Program");
 	}
 
 }
